aea3c8b366b55fc1ac6cc1f8469920544f91edc7
Jefferson Freitas <ajeffsoft@gmail.com>
2020-05-03 15:02:43 -0300

    First commit of Clindor Initiliz

---
+++ src/main/java/com/ajeff/clindor/ClindorApplication.java
@@ -0,0 +1,13 @@
+package com.ajeff.clindor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClindorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClindorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ajeff/clindor/ClindorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajeff.clindor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClindorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
